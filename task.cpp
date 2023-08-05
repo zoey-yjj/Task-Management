b4f46c07a04a1f9eff09a1b78076217bd06be5b8
zoey-yjj <zoeyyyjj@gmail.com>
2023-08-05 12:39:42 +0000

    add role for TeamMember, create team members and tasks in main

--- task.cpp
+++ task.cpp
@@ -5,13 +5,15 @@
 // Define classes for team members and tasks
 class TeamMember {
 public:
-    TeamMember(const std::string& name)
-        : name(name){}
+    TeamMember(const std::string& name, const std::string& role)
+        : name(name), role(role) {}
 
     std::string getName() const { return name; }
+    std::string getRole() const { return role; }
 
 private:
     std::string name;
+    std::string role;
 };
 
 class Task {
@@ -26,6 +28,18 @@ private:
 };
 
 int main() {
+    // Create team members with leader, senior, and juniors
+    TeamMember leader("Team Leader", "Leader");
+    TeamMember senior("Senior Developer", "Senior");
+    std::vector<TeamMember> juniors;
+    juniors.push_back(TeamMember("Junior Developer 1", "Junior"));
+    juniors.push_back(TeamMember("Junior Developer 2", "Junior"));
+    juniors.push_back(TeamMember("Junior Developer 3", "Junior"));
+
+    // Create tasks
+    Task task1("Task 1");
+    Task task2("Task 2");
+    Task task3("Task 3");
 
     return 0;
 }
